dc3c3ddb288b8dd06a83987e9c0a1df3e5a89b95
Jhonnatan Espinoza Rojas <jhonnatan.espinoza.r@uni.pe>
2024-06-01 16:10:11 -0500

    doc: se agrego documentacion

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cración de API REST con Flask
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+o
+
+```bash
+pip install Flask flask-cors python-dotenv google-generativeai
+```
--- app/gemini_api.py
+++ app/gemini_api.py
@@ -12,12 +12,13 @@ def get_suggested_title(items):
     model = genai.GenerativeModel('gemini-1.5-flash', 
         generation_config={
             "response_mime_type": "application/json",
-            "temperature": 0.7,
+            "temperature": 0.5,
             }
         )
-
+    # Crear el prompt para la generación de títulos sugeridos
     prompt = f"List 5 non-specific suggestions for a task title that includes the following elements: {items}."
 
+    # Generar títulos sugeridos en formato JSON
     format_prompt = prompt + """
     
     The suggested titles should be in the language in which the elements are written.
@@ -29,8 +30,10 @@ def get_suggested_title(items):
     Return a `list[Title]`
     """
 
+    # Generar contenido con el modelo generativo
     response = model.generate_content(format_prompt)
     # response = "[{"title": "suggested title 1"},...]"
+    # loads <- convierte un string en un diccionario
     data = json.loads(response.text)
     
     return data
